924721d702e1e430bc60656b46978a4907a1c8d8
anthony fernando torres detquizan <anthonyfernandotorresdetquizan@MacBook-Pro-de-anthony-1027.local>
2025-05-15 09:59:02 -0500

    feat: prisma

--- src/personal/personal.module.ts
+++ src/personal/personal.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { PersonalController } from './personal.controller';
 import { PersonalService } from './personal.service';
 
